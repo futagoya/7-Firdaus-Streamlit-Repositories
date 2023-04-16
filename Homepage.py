e251dd046b9bebc6bc639326d163ea6f257b453a
futagoya <112046437+futagoya@users.noreply.github.com>
2023-04-16 14:29:04 +0700

    Update Homepage.py

--- Homepage.py
+++ Homepage.py
@@ -1,15 +1,28 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Apr 15 14:00:29 2023
-
-@author: ASUS
-"""
-
 import streamlit as st
+from pages import heart_disease_visualization, predict_cost_machine_learning, data_uploader
+from style import set_background
+set_background()
 
-st.set_page_config(
-    page_title="Firdaus Repositories")
+# Create a dropdown menu with the page options
+menu = st.selectbox('Select a page', ['Main Page', 'Heart Disease Visualization', 'Predict Cost Machine Learning', 'Data Uploader'])
 
-st.title('Main Page')
-st.sidebar.success('Select a page above')
+# Show the selected page based on the dropdown menu option
+if menu == 'Main Page':
+    set_background()
+    st.title('Main Page')
+    st.subheader('Welcome to the main page!')
+    st.write('These web app contains some of streamlit projects')
+    st.write('Please select menu above to visit those pages!')
+    st.write('Thank you 😄')
+    if st.checkbox('Created by'):
+        st.success('Firdaus Wahyu Nugroho')
 
+elif menu == 'Heart Disease Visualization':
+    set_background()
+    heart_disease_visualization.show()
+elif menu == 'Predict Cost Machine Learning':
+    set_background()
+    predict_cost_machine_learning.show()
+elif menu == 'Data Uploader':
+    set_background()
+    data_uploader.show()
